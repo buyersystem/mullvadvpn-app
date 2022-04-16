9fd1850846e34cc374daf60d84a7d7769a7c19f2
Andrej Mihajlov <and@mullvad.net>
2022-04-16 17:03:23 +0200

    REST: add access token manager

---
+++ ios/MullvadVPN/REST/RESTAccessTokenManager.swift
@@ -0,0 +1,168 @@
+//
+//  RESTAccessTokenManager.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 16/04/2022.
+//  Copyright © 2022 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import Logging
+import UIKit
+
+extension REST {
+
+    final class AccessTokenManager: RESTAccessTokenStore {
+        private let operationQueue = OperationQueue()
+        private let dispatchQueue = DispatchQueue(label: "REST.AccessTokenManager.dispatchQueue")
+        private let proxy: AuthenticationProxy
+        private var tokens: [String: AccessTokenData] = [:]
+
+        init(authenticationProxy: AuthenticationProxy) {
+            proxy = authenticationProxy
+            operationQueue.maxConcurrentOperationCount = 1
+        }
+
+        func getAccessToken(
+            accountNumber: String,
+            completionHandler: @escaping (OperationCompletion<AccessTokenData, REST.Error>) -> Void
+        ) -> Cancellable
+        {
+            let operation = GetAccessTokenOperation(
+                dispatchQueue: dispatchQueue,
+                proxy: proxy,
+                store: self,
+                accountNumber: accountNumber,
+                completionQueue: .main,
+                completionHandler: completionHandler
+            )
+
+            operationQueue.addOperation(operation)
+
+            return operation
+        }
+
+        fileprivate func getAccessTokenData(accountNumber: String) -> REST.AccessTokenData? {
+            dispatchPrecondition(condition: .onQueue(dispatchQueue))
+
+            return tokens[accountNumber]
+        }
+
+        fileprivate func setAccessTokenData(accountNumber: String, tokenData: REST.AccessTokenData) {
+            dispatchPrecondition(condition: .onQueue(dispatchQueue))
+
+            tokens[accountNumber] = tokenData
+        }
+    }
+
+}
+
+fileprivate protocol RESTAccessTokenStore {
+    func getAccessTokenData(accountNumber: String) -> REST.AccessTokenData?
+    func setAccessTokenData(accountNumber: String, tokenData: REST.AccessTokenData)
+}
+
+extension REST {
+    fileprivate class GetAccessTokenOperation: ResultOperation<AccessTokenData, REST.Error> {
+        private let dispatchQueue: DispatchQueue
+        private let proxy: AuthenticationProxy
+        private let store: RESTAccessTokenStore
+        private let accountNumber: String
+        private let logger = Logger(label: "REST.GetAccessTokenOperation")
+        private var proxyTask: Cancellable?
+
+        init(
+            dispatchQueue: DispatchQueue,
+            proxy: AuthenticationProxy,
+            store: RESTAccessTokenStore,
+            accountNumber: String,
+            completionQueue: DispatchQueue?,
+            completionHandler: CompletionHandler?
+        )
+        {
+            self.dispatchQueue = dispatchQueue
+            self.proxy = proxy
+            self.store = store
+            self.accountNumber = accountNumber
+
+            super.init(
+                completionQueue: completionQueue,
+                completionHandler: completionHandler
+            )
+        }
+
+        override func main() {
+            dispatchQueue.async {
+                guard !self.isCancelled else {
+                    self.finish(completion: .cancelled)
+                    return
+                }
+
+                guard let tokenData = self.store.getAccessTokenData(accountNumber: self.accountNumber) else {
+                    self.obtainAccessToken()
+                    return
+                }
+
+                guard tokenData.expiry > Date() else {
+                    self.refreshAccessToken(tokenData.accessToken)
+                    return
+                }
+
+                self.finish(completion: .success(tokenData))
+            }
+        }
+
+        override func cancel() {
+            super.cancel()
+
+            dispatchQueue.async {
+                self.proxyTask?.cancel()
+                self.proxyTask = nil
+            }
+        }
+
+        private func obtainAccessToken() {
+            proxyTask = proxy.getAccessToken(accountNumber: accountNumber) { completion in
+                self.dispatchQueue.async {
+                    switch completion {
+                    case .success(let tokenData):
+                        self.store.setAccessTokenData(accountNumber: self.accountNumber, tokenData: tokenData)
+
+                    case .failure(let error):
+                        self.logger.error(
+                            chainedError: error,
+                            message: "Failed to obtain access token."
+                        )
+
+                    case .cancelled:
+                        break
+                    }
+
+                    self.finish(completion: completion)
+                }
+            }
+        }
+
+        private func refreshAccessToken(_ accessToken: String) {
+            proxyTask = proxy.refreshAccessToken(accessToken: accessToken) { completion in
+                self.dispatchQueue.async {
+                    switch completion {
+                    case .success(let tokenData):
+                        self.store.setAccessTokenData(accountNumber: self.accountNumber, tokenData: tokenData)
+
+                    case .failure(let error):
+                        self.logger.error(
+                            chainedError: error,
+                            message: "Failed to refresh access token."
+                        )
+
+                    case .cancelled:
+                        break
+                    }
+
+                    self.finish(completion: completion)
+                }
+            }
+        }
+    }
+}
